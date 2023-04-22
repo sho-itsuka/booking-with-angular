b5ba17469e0a3df7452c5195f1332f3f3c8b92f6
sho-itsuka <sho.itsuka122@gmail.com>
2023-04-22 23:09:55 +0900

    homeコンポーネント追加

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SignInPageComponent } from './components/sign-in-page/sign-in-page.component';
+import { HomeComponent } from './components/home/home.component';
 
 
 
 @NgModule({
   declarations: [
-    SignInPageComponent
+    SignInPageComponent,
+    HomeComponent
   ],
   imports: [
     CommonModule
