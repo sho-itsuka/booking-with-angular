bc73dfae9ef0f59616604848dd68f618088b1409
sho-itsuka <sho.itsuka122@gmail.com>
2023-04-22 20:44:17 +0900

    navigateメソッドの定義

--- src/app/core/services/routing.service.ts
+++ src/app/core/services/routing.service.ts
@@ -1,3 +1,5 @@
+import { UrlConst } from 'src/app/pages/constants/url-const';
+
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
 
@@ -9,5 +11,6 @@ export class RoutingService {
   constructor(public router: Router) { }
 
   public navigate(path: string) {
+    this.router.navigate([UrlConst.SLASH + path])
   }
 }
