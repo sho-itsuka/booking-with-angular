0501847c41415503c95bf81d75c3583748e6c9e3
sho-itsuka <sho.itsuka122@gmail.com>
2023-04-22 16:05:15 +0900

    ロケールの取り込み

---
+++ src/app/ngx-translate/ngx-translate.module.ts
@@ -0,0 +1,30 @@
+import { CommonModule, registerLocaleData } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import localeJa from '@angular/common/locales/ja';
+import { NgModule } from '@angular/core';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+
+registerLocaleData(localeJa);
+
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http);
+}
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient]
+      }
+    })
+  ],
+  exports: [
+    TranslateModule
+  ]
+})
+export class NgxTranslateModule { }
