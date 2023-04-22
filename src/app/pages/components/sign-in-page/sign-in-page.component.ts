eaf49149b40d7fd374332f79b28157c1c2e8a233
sho-itsuka <sho.itsuka122@gmail.com>
2023-04-22 20:30:57 +0900

    定数を定義

--- src/app/pages/components/sign-in-page/sign-in-page.component.ts
+++ src/app/pages/components/sign-in-page/sign-in-page.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-sign-in-page',
+  selector:    'app-sign-in-page',
   templateUrl: './sign-in-page.component.html',
-  styleUrls: ['./sign-in-page.component.scss']
+  styleUrls:  ['./sign-in-page.component.scss']
 })
 export class SignInPageComponent {
 
