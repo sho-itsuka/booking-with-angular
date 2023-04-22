4193218b1efc09ac02d112b651e036fa979bb2f0
sho-itsuka <sho.itsuka122@gmail.com>
2023-04-22 23:14:27 +0900

    homeページへのルーティング

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+import { HomeComponent } from './pages/components/home/home.component';
 import { SignInPageComponent } from './pages/components/sign-in-page/sign-in-page.component';
 import { UrlConst } from './pages/constants/url-const';
 
 const routes: Routes = [
   { path: '', redirectTo: UrlConst.SLASH + UrlConst.PATH_SIGN_IN, pathMatch: 'full' },
-  { path: UrlConst.PATH_SIGN_IN, component: SignInPageComponent }
+  { path: UrlConst.PATH_SIGN_IN, component: SignInPageComponent },
+  { path: UrlConst.PATH_HOME,    component: HomeComponent }
 ]
 
 @NgModule({
