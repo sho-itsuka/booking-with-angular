4de162c367bc3554f2239091e0072c753df9ea6e
sho-itsuka <sho.itsuka122@gmail.com>
2023-04-27 00:23:23 +0900

    タイトルの多言語化

--- src/app/pages/components/home/home.component.ts
+++ src/app/pages/components/home/home.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { TitleI18Service } from 'src/app/shared/services/title-i18.service';
+
+import { AfterViewChecked, Component, OnInit } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
+
+import { UrlConst } from '../../constants/url-const';
 
 @Component({
   selector:    'app-home',
   templateUrl: './home.component.html',
   styleUrls:  ['./home.component.scss']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit, AfterViewChecked {
 
+  constructor(
+    private translateService: TranslateService,
+    private titleI18Service:  TitleI18Service,
+  ) {}
+
+  /**
+   * OnInit
+   */
+  ngOnInit(): void {
+  }
+
+  /**
+   * after view checked
+   */
+  ngAfterViewChecked(): void {
+    this.titleI18Service.setTitle(UrlConst.PATH_HOME);
+  }
 }
